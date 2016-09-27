dfb6d156a54c41596f661da77a53169c2d55db66
Amr <amrsfmt@yahoo.com>
2016-09-27 00:03:41 -0500

    - Update to the ignore file.

--- example.py
+++ example.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
-from python_speech_features import mfcc
-from python_speech_features import logfbank
+from python_speech_features import *
+
 import scipy.io.wavfile as wav
 
 (rate,sig) = wav.read("english.wav")
+
+plot_mono_signal(sig,rate)
+
 mfcc_feat = mfcc(sig,rate)
 fbank_feat = logfbank(sig,rate)
 
-print(fbank_feat[1:3,:])
+print mfcc_feat.shape
+print mfcc_feat
+
+plot_mfcc(mfcc_feat)
